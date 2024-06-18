3a623749b1a30f517c106d8bcfad396793e35096
Maiara Santos <maiarasantos34@yahoo.com>
2024-06-18 17:35:59 -0300

    Add files via upload

---
+++ dist/desafio4.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Validações
+const list_of_packs = [];
+list_of_packs.push('um');
+list_of_packs.push(2);
+list_of_packs.push(2.5);
